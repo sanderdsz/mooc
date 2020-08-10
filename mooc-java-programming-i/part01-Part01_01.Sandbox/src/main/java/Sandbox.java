c5b70782a1ebb071665f14d86c662d91cd1241d7
sanderdsz <sanderdsz@gmail.com>
2020-08-10 20:33:29 -0300

    first commit

---
+++ mooc-java-programming-i/part01-Part01_01.Sandbox/src/main/java/Sandbox.java
@@ -0,0 +1,8 @@
+
+public class Sandbox {
+
+    public static void main(String[] args) {
+        // Here goes the statements used by the program
+        System.out.println("Hello world!");
+    }
+}
---
+++ mooc-java-programming-i/part01-Part01_02.AdaLovelace/src/main/java/AdaLovelace.java
@@ -0,0 +1,9 @@
+
+public class AdaLovelace {
+
+    public static void main(String[] args) {
+        // Here goes the statements used by the program
+        System.out.println("Ada Lovelace");
+        
+    }
+}
