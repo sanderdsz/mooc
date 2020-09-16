268653a184b65231cd7cd2dd73aa87f49d974ad8
Sander dos Santos Zuchinalli <sanderdsz@gmail.com>
2020-09-16 10:12:05 -0300

    part 1 calculating 1

--- mooc-java-programming-i/part01-Part01_16.SecondsInADay/src/main/java/SecondsInADay.java
+++ mooc-java-programming-i/part01-Part01_16.SecondsInADay/src/main/java/SecondsInADay.java
@@ -6,7 +6,9 @@ public class SecondsInADay {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Write your program here
-
+        System.out.println("How many days would you like to convert to seconds?");
+        int number = Integer.valueOf(scanner.nextLine());
+        int calculation = (((number * 24) * 60) * 60);
+        System.out.println(calculation);
     }
 }
