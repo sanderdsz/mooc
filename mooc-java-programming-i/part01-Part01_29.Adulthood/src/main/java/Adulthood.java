088f60c71cf2f7bf5fe5cda63e96f4b473122cd5
sanderdsz <sanderdsz@gmail.com>
2020-10-05 10:39:52 -0300

    part 1 conditionals largerthan

--- mooc-java-programming-i/part01-Part01_29.Adulthood/src/main/java/Adulthood.java
+++ mooc-java-programming-i/part01-Part01_29.Adulthood/src/main/java/Adulthood.java
@@ -5,9 +5,9 @@ public class Adulthood {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("How old are you?");
-        int number = Integer.valueOf(scanner.nextLine());
-        if (number < 18) {
+        System.out.println("Give the first number:");
+        int first = Integer.valueOf(scanner.nextLine());
+        if (first < 18) {
             System.out.println("You are not an adult");
         } else {
             System.out.println("You are an adult");
--- mooc-java-programming-i/part01-Part01_30.LargerThanOrEqualTo/src/main/java/LargerThanOrEqualTo.java
+++ mooc-java-programming-i/part01-Part01_30.LargerThanOrEqualTo/src/main/java/LargerThanOrEqualTo.java
@@ -4,7 +4,17 @@ import java.util.Scanner;
 public class LargerThanOrEqualTo {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Give the first number:");
+        int first = Integer.valueOf(scanner.nextLine());
+        System.out.println("Give the second number:");
+        int second = Integer.valueOf(scanner.nextLine());
+        if (first > second) {
+            System.out.println("Greater number is: " + first);
+        } else if (first < second) {
+            System.out.println("Greater number is: " + second);
+        } else {
+            System.out.println("The numbers are equal!");
+        }
     }
 }
