108938760532173a7c8ab3bcd82c9cf2c0d3f979
sander.zuchinalli <sander.zuchinalli@betha.com.br>
2020-10-05 10:32:05 -0300

    part 1 conditionals adulthood

--- mooc-java-programming-i/part01-Part01_29.Adulthood/src/main/java/Adulthood.java
+++ mooc-java-programming-i/part01-Part01_29.Adulthood/src/main/java/Adulthood.java
@@ -4,8 +4,13 @@ import java.util.Scanner;
 public class Adulthood {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-
-        // Write your program here 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How old are you?");
+        int number = Integer.valueOf(scanner.nextLine());
+        if (number < 18) {
+            System.out.println("You are not an adult");
+        } else {
+            System.out.println("You are an adult");
+        }
     }
 }
