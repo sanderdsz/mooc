1f4906b2c891b7026456983880b7635ddd90c8c2
sanderdsz <sanderdsz@gmail.com>
2020-10-09 11:30:09 -0300

    oddeven

--- mooc-java-programming-i/part01-Part01_32.OddOrEven/src/main/java/OddOrEven.java
+++ mooc-java-programming-i/part01-Part01_32.OddOrEven/src/main/java/OddOrEven.java
@@ -4,7 +4,16 @@ import java.util.Scanner;
 public class OddOrEven {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner reader = new Scanner(System.in);
+        int number = Integer.valueOf(reader.nextLine());
+        
+        int remainder = number % 2;
+
+        if (remainder == 0) {
+            System.out.println("The number " + number + " is even.");
+        } else {
+            System.out.println("The number " + number + " is odd.");
+        }
 
         // Write your program here 
         // HINT:
