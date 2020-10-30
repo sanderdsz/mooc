f3ca9168a0bf00c5a9e93080b3ac9cf87aa3e312
sanderdsz <sanderdsz@gmail.com>
2020-10-29 21:13:29 -0300

    part 1 done

--- mooc-java-programming-i/part01-Part01_37.GiftTax/src/main/java/GiftTax.java
+++ mooc-java-programming-i/part01-Part01_37.GiftTax/src/main/java/GiftTax.java
@@ -4,7 +4,28 @@ import java.util.Scanner;
 public class GiftTax {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner reader = new Scanner(System.in);
 
+        System.out.println("Value of the gift?");
+        float gift = Float.parseFloat(reader.nextLine());
+
+        if (gift < 5000) {
+            System.out.println("No tax!");
+        } else if (gift >= 5000 && gift < 25000) {
+            double result = (100 + ((gift - 5000) * 0.080));
+            System.out.println("Tax: " + result);
+        } else if (gift >= 25000 && gift < 55000) {
+            double result = (1700 + ((gift - 25000) * 0.10));
+            System.out.println("Tax: " + result);
+        } else if (gift >= 55000 && gift < 200000) {
+            double result = (4700 + ((gift - 55000) * 0.120));
+            System.out.println("Tax: " + result);
+        } else if (gift >= 200000 && gift < 1000000) {
+            double result = (22100 + ((gift - 200000) * 0.150));
+            System.out.println("Tax: " + result);
+        } else {
+            double result = (142100 + ((gift - 1000000) * 0.170));
+            System.out.println("Tax: " + result);
+        }
     }
 }
