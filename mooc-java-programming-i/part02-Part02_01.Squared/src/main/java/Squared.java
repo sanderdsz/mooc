d97d0c75740bd2375a070719d420f3f742cdaae4
sanderdsz <sanderdsz@gmail.com>
2020-11-16 17:44:47 -0300

    part2-1

--- mooc-java-programming-i/part02-Part02_01.Squared/src/main/java/Squared.java
+++ mooc-java-programming-i/part02-Part02_01.Squared/src/main/java/Squared.java
@@ -4,7 +4,14 @@ import java.util.Scanner;
 public class Squared {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner reader = new Scanner(System.in);
+
+        int number = Integer.valueOf(reader.nextLine());
+
+        int square = number * number;
+
+        // Printing the result of the operation
+        System.out.println(square);
 
     }
 }
