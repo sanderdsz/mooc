3448a5462bfbefa1019694c2d707dea67eb29f4a
sanderdsz <sanderdsz@gmail.com>
2021-07-27 22:58:23 -0300

    part 3 - 27

--- mooc-java-programming-i/part03-Part03_27.AVClub/src/main/java/AVClub.java
+++ mooc-java-programming-i/part03-Part03_27.AVClub/src/main/java/AVClub.java
@@ -5,7 +5,19 @@ public class AVClub {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String text = scanner.nextLine();
+            if (text.isEmpty()) {
+                break;
+            }
 
+            String[] pieces = text.split(" ");
+            for (String piece : pieces) {
+                if (piece.contains("av")) {
+                    System.out.println(piece);
+                }
+            }
+        }
 
     }
 }
