3ab59e989b7ff01fc89e8ecf9f515c29bfb5e04e
sanderdsz <sanderdsz@gmail.com>
2021-07-27 22:28:06 -0300

    part 3 - 26

--- mooc-java-programming-i/part03-Part03_26.LineByLine/src/main/java/LineByLine.java
+++ mooc-java-programming-i/part03-Part03_26.LineByLine/src/main/java/LineByLine.java
@@ -5,7 +5,16 @@ public class LineByLine {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-
+        while (true) {
+            String text = scanner.nextLine();
+            if (text.isEmpty()) {
+                break;
+            }
+            String[] lines = text.split(" ");
+            for (int i = 0; i < lines.length; i++) {
+                System.out.println(lines[i]);
+            }
+        }
+        
     }
 }
