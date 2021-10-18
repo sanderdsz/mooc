0d6c6ec44de9d281b1fd3f553eefa00b8bb4f2cc
sanderdsz <sanderdsz@gmail.com>
2021-10-18 19:52:24 -0300

    part 3 - 30

--- mooc-java-programming-i/part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
+++ mooc-java-programming-i/part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
@@ -1,11 +1,29 @@
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class AgeOfTheOldest {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-
+        int max = 0;
+        int count = 0;
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("")) {
+                break;
+            }
+            String[] parts = input.split(",");
+            list.add(Integer.valueOf(parts[1]));
+            count = count + 1;
+        }
+        
+        if (count > 0) {
+            max = Collections.max(list);
+            System.out.println("Age of the oldest: " + max);
+        }
     }
 }
