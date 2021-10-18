22670fb5d8dfda69b0a808895ccad6b1308bd364
sanderdsz <sanderdsz@gmail.com>
2021-10-18 20:53:45 -0300

    part 3 - 32

--- mooc-java-programming-i/part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
+++ mooc-java-programming-i/part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
@@ -1,11 +1,34 @@
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class NameOfTheOldest {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-
+        int maxVal = 0;
+        int maxIndex = 0;
+        int count = 0;
+        ArrayList<String> names = new ArrayList<String>();
+        ArrayList<Integer> ages = new ArrayList<Integer>();
+        
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("")) {
+                break;
+            }
+            String[] parts = input.split(",");
+            names.add(parts[0]);
+            ages.add(Integer.valueOf(parts[1]));
+            count = count + 1;
+        }
+        
+        if (count > 0) {
+            maxVal = Collections.max(ages);
+            maxIndex = ages.indexOf(maxVal);
+            String name = names.get(maxIndex);
+            System.out.println("Name of the oldest: " + name);
+        }
     }
 }
--- mooc-java-programming-i/part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
+++ mooc-java-programming-i/part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
@@ -1,12 +1,43 @@
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class PersonalDetails {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-
+        int count = 0;
+        int sum = 0;
+        BigDecimal avg;
+        ArrayList<String> names = new ArrayList<String>();
+        ArrayList<Integer> ages = new ArrayList<Integer>();
+        
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("")) {
+                break;
+            }
+            String[] parts = input.split(",");
+            names.add(parts[0]);
+            ages.add(Integer.valueOf(parts[1]));
+            count = count + 1;
+        }
+        
+        if (count > 0) {
+            String name = Collections.max(names, Comparator.comparing(String::length));
+            System.out.println("Longest name: " + name);
+            for (int i = 0; i < ages.size(); i++) {
+                sum += ages.get(i);
+            }
+            avg = BigDecimal.valueOf(sum).divide(BigDecimal.valueOf(ages.size()));
+            System.out.println(name);
+            System.out.println(sum);
+            System.out.println(ages.size());
+            System.out.println(avg);
+            System.out.println("Average of the birth years: " + avg.setScale(1));
+        }
     }
 }
