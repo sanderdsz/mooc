2d446862a04d8833f350ede254c8cfea4f187070
Sander <sanderdsz@gmail.com>
2022-10-23 18:32:03 -0300

    part 04-01

--- mooc-java-programming-i/part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
+++ mooc-java-programming-i/part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -4,5 +4,11 @@ public class YourFirstAccount {
     public static void main(String[] args) {
         // Do not touch the code in Account.java
         // Write your program here
+        
+        Account sanderAccount = new Account("Sander's account", 100.00);
+        
+        sanderAccount.deposit(20.00);
+
+        System.out.println(sanderAccount);
     }
 }
