5b205cbe97cbbaab49e256da5d572812609f202b
sanderdsz <sanderdsz@gmail.com>
2023-04-03 21:02:29 -0300

    Part04 05-11

---
+++ mooc-java-programming-i/part04-Part04_07.Product/src/main/java/Product.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author sander
+ */
+public class Product {
+    
+    private double price;
+    
+    private int quanity;
+    
+    private String name;
+    
+    public Product(String initialName, double initialPrice, int initialQuantity) {
+        this.price = initialPrice;
+        this.name = initialName;
+        this.quanity = initialQuantity;
+    }
+    
+    public void printProduct() {
+        System.out.println(this.name + ", price " + this.price + ", " + this.quanity);
+    }
+    
+}
