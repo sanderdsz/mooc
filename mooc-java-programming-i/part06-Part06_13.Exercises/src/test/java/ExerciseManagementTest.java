b50523baaec07d8ec701649c3d857999aa722458
sander <sanderdsz@gmail.com>
2023-10-23 20:37:16 -0300

    part 06 testing

--- mooc-java-programming-i/part06-Part06_13.Exercises/src/test/java/ExerciseManagementTest.java
+++ mooc-java-programming-i/part06-Part06_13.Exercises/src/test/java/ExerciseManagementTest.java
@@ -1,6 +1,47 @@
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
 
 public class ExerciseManagementTest {
-
+    
+    private ExerciseManagement management;
+    
+    @Before
+    public void initialize() {
+        management = new ExerciseManagement();
+    }
+    
+    @Test
+    public void exerciseListEmptyAtBeginning() {
+        Assert.assertEquals(0, management.exerciseList().size());
+    }
+    
+    @Test
+    public void addingExerciseGrowsListByOne() {
+        management.add("Write a test");
+        Assert.assertEquals(1, management.exerciseList().size());
+    }
+    
+    @Test
+    public void addedExerciseInList() {
+        management.add("Write a test");
+        Assert.assertTrue(management.exerciseList().contains("Write a test"));
+    }
+    
+    @Test
+    public void exerciseCanBeMarkedAsCompleted() {
+        management.add("New exercise");
+        management.markAsCompleted("New exercise");
+        Assert.assertTrue(management.isCompleted("New exercise"));
+    }
+    
+    @Test
+    public void ifNotMarkedCompletedIsNotCompleted() {
+        management.add("New exercise");
+        management.markAsCompleted("New exercise");
+        Assert.assertFalse(management.isCompleted("Some exercise"));
+    }
+    
 }
